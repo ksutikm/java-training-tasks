ab42277185da934c4cf99d28663b6ff56d0fe1e8
ksutikm <tsksusha@yandex.ru>
2022-03-30 16:05:03 +0300

    Fix code

--- AnnotationTask/src/Container.java
+++ AnnotationTask/src/Container.java
@@ -4,29 +4,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Container {
-    public Object createInstance(Class clazz) throws IllegalArgumentException {
+    public <T> T createInstance(Class<T> clazz) throws IllegalArgumentException {
         if (clazz.isAnnotationPresent(ControlledObject.class)){
             System.out.println("create instance ; name  -  " + clazz.getAnnotation(ControlledObject.class));
         } else {
             throw new IllegalArgumentException("Class is not annotated");
         }
 
-        Method method = null;
+        Method method = getInitMethod(clazz);
+        T obj = null;
+
+        try {
+            obj = clazz.newInstance();
+            method.invoke(obj);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+        return obj;
+    }
+
+    private <T> Method getInitMethod(Class<T> clazz) {
         List<Method> methods = Arrays.stream(clazz.getMethods())
                 .filter(value -> value.isAnnotationPresent(StartObject.class))
                 .collect(Collectors.toList());
         if (methods.size() > 1) {
             throw new IllegalArgumentException("This class must have one method with StartObject annotation");
-        } else {
-            method = methods.get(0);
         }
-
-        try {
-            method.invoke(clazz.newInstance());
-            return clazz.newInstance();
-        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return methods.get(0);
     }
 }
--- AnnotationTask/src/Main.java
+++ AnnotationTask/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
-        HelloWord helloWord = (HelloWord) container.createInstance(HelloWord.class);
+        HelloWord helloWord = container.createInstance(HelloWord.class);
     }
 }
