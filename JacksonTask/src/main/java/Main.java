0c9b9a285cb1d70555c74b7d91b7866a89cc8ba4
ksutikm <tsksusha@yandex.ru>
2022-04-01 16:58:22 +0300

    Add solve jackson for list

--- JacksonTask/src/main/java/Main.java
+++ JacksonTask/src/main/java/Main.java
@@ -1,20 +1,25 @@
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException, IOException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Car car = new Car("Audi", "A6", dateFormat.parse("2011-10-01"));
+        List<Car> cars = new ArrayList<Car>();
+        cars.add(new Car("Audi", "A6", dateFormat.parse("2011-10-01")));
+        cars.add(new Car("Honda", "Civic", dateFormat.parse("2012-01-20")));
 
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(car);
-        System.out.printf("JSON serialization: %s\n\n", json);
+        String jsonList = mapper.writeValueAsString(cars);
+        System.out.printf("JSON serialization: %s\n\n", jsonList);
 
-        Car carFromJson = mapper.readValue(json, Car.class);
-        System.out.printf("JSON deserialization: %s", carFromJson);
+        List<Car> carsFromJsonList = mapper.readValue(jsonList, new TypeReference<List<Car>>() { });
+        System.out.printf("JSON deserialization: %s", carsFromJsonList);
     }
 }
