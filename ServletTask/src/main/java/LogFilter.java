b7e96ff4e712f18251d9565a4c8d6f144992761d
ksutikm <tsksusha@yandex.ru>
2022-04-08 15:20:35 +0300

    Fix code

--- ServletTask/src/main/java/LogFilter.java
+++ ServletTask/src/main/java/LogFilter.java
@@ -8,26 +8,15 @@ import java.io.IOException;
 
 @WebFilter("/table")
 public class LogFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-        System.out.println("LogFilter init!");
-    }
+    private static final Logger LOGGER = Logger.getLogger(LogFilter.class.getName());
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String action = request.getParameter("action");
-        Logger logger = Logger.getLogger(LogFilter.class.getName());
 
         String message = (action.equals("get")) ? "Entries" : "Entry";
-        logger.log(Level.INFO, String.format("%s %s", message, action));
+        LOGGER.log(Level.INFO, String.format("%s %s", message, action));
 
         chain.doFilter(request, response);
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-        System.out.println("LogFilter destroy!");
     }
 }
